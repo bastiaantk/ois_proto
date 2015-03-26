33482e6e120a5a41c2daf1a0328ef5fcdd5b714a
bastiaantk <bazzytk@live.nl>
2015-03-26 11:41:16 +0100

    Created file strucutre

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Universiteit Utrecht - Onderwijs informatica en informatiekunde 2014/2015</title>
+	</head>
+	<body>
+	
+	</body>
+</html>
