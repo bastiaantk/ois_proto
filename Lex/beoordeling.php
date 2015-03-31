6c880c2d826d783b9c8092012256a345d0fc8240
lexnagtegaal <lex.ook.mail@gmail.com>
2015-03-31 22:01:41 +0200

    Opdrachten
    Opdrachten.php met daarbij 3 bestanden, beoordeling.php inleveren.php en
    uitleg.php die de verschillende pagina's van opdrachten uitwerkt. Geen
    styling gedaan. Alleen via HTML commentaar toegevoegd bij een enkele
    list dat die list inline block moet. (Geldt dus voor alle List binnen
    die UL> en bij de div van klasse downloads en en klasse commentaar
    aangegeven dat ze float left en right moeten zijn. De overige moet je
    even goed kijken naar het storyboard, maar spreekt volgens mij redelijk
    voor zich.

---
+++ Lex/beoordeling.php
@@ -0,0 +1,54 @@
+<h1>Beoordeling</h1>
+<div class="overzicht">
+	<table>
+		<tr>
+			<td>Cijfer:</td>
+			<td>N.N.B.</td>
+		</tr>
+		<tr>
+			<td>Inleverdatum:</td>
+			<td>20 April 2015</td>
+		</tr>
+		<tr>
+			<td>Mutatiedatum:</td>
+			<td><?php echo date(); ?></td>
+		</tr>
+		<tr>
+			<td>Gemiddelde:</td>
+			<td>N.N.B.</td>
+		</tr>
+		<tr>
+			<td>Weging:</td>
+			<td>40%</td>
+		</tr>
+		<tr>
+			<td>Beste:</td>
+			<td>N.N.B.</td>
+		</tr>
+	</table>
+</div>
+<div class="downloads"> <!-- Float Left -->
+	<ul>
+		<li>Opdracht1.word</li>
+		<li>Correctie_Opdracht1.pdf</li>
+	</ul>
+</div>
+<div class="opmerkingen"> <!-- Floar Right -->
+	<p class="wie">Lennart</p>
+	<p>Ik heb bestand nagekeken. Deze is terug te vinden als Correctie_opdracht1.pdf onder de downloads</p>
+<?php
+	foreach ($_POST['commentaar'] as $commentaar){
+		echo "<p class=\"wie\">Ik</p>";
+		echo "<p class=\"wat\">$commentaar</p>";
+	}
+?>
+<form method="post">
+<?php
+	foreach ($_POST['commentaar'] as $commentaar){
+		echo "<input type=\"hidden\" name=\"commentaar[]\" value=\"$commentaar\">";
+	}
+?>
+	<textarea name="commentaar[]"></textarea>
+	<input type="submit" value="Reageren">
+</form>
+</div>
